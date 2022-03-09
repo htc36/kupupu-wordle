2cff23b09a43251832f3d8862d1f4eedd21268e7
Harry Collard <harry@gocloud.systems>
2022-03-10 08:58:02 +1300

    Added basic navigation

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,32 @@
 <script setup lang="ts">
 import WordleGame from './WordleGame.vue';
 import CardGame from './CardGame.vue';
+import { computed, ref } from 'vue';
+interface Routes {
+  '/': InstanceType<typeof CardGame>;
+  '/wordle': InstanceType<typeof WordleGame>;
+}
+
+const routes = {
+  '/': CardGame,
+  '/wordle': WordleGame,
+};
+
+const currentPath = ref(window.location.hash);
+
+window.addEventListener('hashchange', () => {
+  currentPath.value = window.location.hash;
+});
+
+const currentView = computed(() => {
+  const path: string = currentPath.value.slice(1) || '/';
+  return routes[path as keyof Routes] || CardGame;
+});
 </script>
 <template>
   <div class="gameWrapper">
-    <CardGame></CardGame>
-    <!-- <WordleGame></WordleGame> -->
+    <a href="#/">Home</a> | <a href="#/wordle">Wordle</a> |
+    <component :is="currentView" />
   </div>
 </template>
 <style scoped>
