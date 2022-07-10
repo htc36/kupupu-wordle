8476af4038943b23261ac4c179f1d730aeefe6a8
Harry Collard <harry@gocloud.systems>
2022-07-10 12:52:43 +1200

    Added about us text

--- src/components/globalComponents/AboutUs.vue
+++ src/components/globalComponents/AboutUs.vue
@@ -5,33 +5,30 @@
       <h1 class="title">Hi I'm Paul Ransfield</h1>
       <p>
         <img
-          src="/assets/card3.png"
+          src="/assets/paul.jpg"
           class="play-pause"
           style="float: right; margin: 0 0 0 15px"
         />
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eget
-        tempus nisi, vitae elementum augue. Maecenas eget ullamcorper leo. Cras
-        aliquam maximus dui, ac maximus odio pellentesque ac. Vestibulum
+        Āe mārika koe – fantastic you, for learning te reo māori. Ka harakoakoa
+        au – I’m delighted that you are using kupupū, kupuhī and kupukō on your
+        learning journey.
       </p>
-    </div>
-    <div>
-      <h1 class="title" style="">What we stand for</h1>
+      <h1 class="title" style="">Learning Material</h1>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eget
-        aliquam ante, quis pharetra ante finibus eget. Ut quis ex
+        Download the free to use, free to share PDF and if these tools help you,
+        kaua e wareware – don’t forget to buy us a coffee.
       </p>
     </div>
   </div>
 </template>
 <style scoped>
 .play-pause {
-  width: 200px;
-  height: 200px;
+  width: 150px;
 }
 .about-us-container {
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
+  justify-content: center;
   height: 100%;
 }
 .top-coloumn-container {
@@ -41,6 +38,7 @@
 }
 .title {
   font-weight: bold;
-  padding-bottom: 5px;
+  padding-bottom: 20px;
+  padding-top: 20px;
 }
 </style>
